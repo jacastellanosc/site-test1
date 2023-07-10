d37d7ddc9c7400d862c79eb1a5a1e95b4b7768cd
Jimmy Castellanos <jacastellanosc@gmail.com>
2023-07-10 05:20:37 -0500

    ....

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Despliegue automático</title>
+    <title>Test _ Despliegue automático</title>
 </head>
 <body>
     Fecha: <?php echo date("Y-m-d H:i:s"); ?>
