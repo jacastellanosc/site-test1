a6c9c2c5960090a2b03c7b7453a80f8fec429d8f
Jimmy Castellanos <jacastellanosc@gmail.com>
2023-07-10 05:19:42 -0500

    Prueba html5

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php 
-echo "Fecha: " . date('Y-m-d H:i:s');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Despliegue automático</title>
+</head>
+<body>
+    Fecha: <?php echo date("Y-m-d H:i:s"); ?>
+</body>
+</html>
