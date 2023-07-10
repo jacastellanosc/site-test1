709325de14fb144436b8fb54a1bedcfb1e89b5f3
Jimmy Castellanos <jacastellanosc@gmail.com>
2023-07-10 05:35:06 -0500

    bootstrap

--- index.php
+++ index.php
@@ -4,15 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test _ Despliegue automático</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-    Fecha: <?php echo date("Y-m-d H:i:s"); ?>
-    <?php 
-    require_once "vendor/autoload.php";
+    <div class="panel panel-primary">
+        <div class="panel-body">
+                Fecha: <?php echo date("Y-m-d H:i:s"); ?>
+                <br />
+                <?php
+                require_once "vendor/autoload.php";
 
-    $hello = new Rivsen\Demo\Hello();
-    echo $hello->hello();
+                $hello = new Rivsen\Demo\Hello();
+                echo $hello->hello();
 
-    ?>
+                ?>
+        </div>
+    </div>
 </body>
 </html>
